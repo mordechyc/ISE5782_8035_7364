320154d3a80e5fa16b60844994872a8e47b160e7
mordechyCohen <mordechyc@gmail.com>
2022-03-06 12:26:45 +0200

    test

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,49 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for primitives.Point class
+ * @author Mordechy Cohen Avichai Kadosh
+ */
+class PointTest {
+
+    /**
+     * Test method for {@link primitives.Point#add(primitives.Vector)}.
+     */
+    @Test
+    public void testAdd() {
+        Point p = new Point(1,2,3);
+
+        // ============ Equivalence Partitions Tests ==============
+
+
+
+        // =============== Boundary Values Tests ==================
+
+
+    }
+
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
+    @Test
+    void subtract() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+     */
+    @Test
+    void distanceSquared() {
+    }
+
+    /**
+     * Test method for {@link primitives.Point#distance(primitives.Point)}.
+     */
+    @Test
+    void distance() {
+    }
+}
