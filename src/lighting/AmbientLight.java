c895f80a51b1375e77ae0649fd4f649d7ca93efd
mordechyCohen <mordechyc@gmail.com>
2022-04-12 11:02:31 +0300

    AmbientLight - JavaDoc

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -4,17 +4,31 @@ import primitives.Color;
 import primitives.Double3;
 
 public class AmbientLight {
-    private Color intensity;
 
+    private Color intensity; //Intensity of ambient light color
 
+    /**
+     * Constructor
+     * @param Ia intensity color
+     * @param Ka constant for intensity
+     */
     public AmbientLight(Color Ia, Double3 Ka) {
         this.intensity = Ia.scale(Ka);
     }
 
+    /**
+     *  default constructor :
+     *  This is a constructor that sets the intensity to black.
+     */
     public AmbientLight() {
         this.intensity = Color.BLACK;
     }
 
+    /**
+     * This function returns the intensity of the light.
+     *
+     * @return The intensity of the light.
+     */
     public Color getIntensity() {
         return intensity;
     }
