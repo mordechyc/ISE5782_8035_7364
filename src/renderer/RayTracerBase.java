e0ee4dd34788a1d856eb9d75b28ad2738513cd78
mordechyCohen <mordechyc@gmail.com>
2022-04-12 12:30:48 +0300

    RayTracerBase - JavaDoc

--- src/renderer/RayTracerBase.java
+++ src/renderer/RayTracerBase.java
@@ -5,11 +5,19 @@ import primitives.Ray;
 import scene.Scene;
 
 public abstract class RayTracerBase {
-    protected Scene scena;
-    public RayTracerBase(Scene scena) {
-        this.scena = scena;
-    }
+
+    protected Scene scena; //objects of Scene
+
     /**
+     * Constructor
+     * @param scene scene
+     */
+    public RayTracerBase(Scene scene) {
+        this.scena = scene;
+    }
+
+    /**
+     * abstract method
      * Trace the ray and calculates the color
      * @param ray the ray that came out of the camera
      * @return the color of the object that the ray is interact with
