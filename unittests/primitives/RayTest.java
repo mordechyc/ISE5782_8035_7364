1cf4d8d2cfc9eca575b222575fbc721ff4440ee6
mordechyCohen <mordechyc@gmail.com>
2022-04-12 12:16:20 +0300

    testFindClosestPoint in primitives.Ray include - JavaDoc

--- unittests/primitives/RayTest.java
+++ unittests/primitives/RayTest.java
@@ -4,57 +4,52 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static primitives.Util.isZero;
+
+import java.util.LinkedList;
 import java.util.List;
 
 class RayTest {
-    Ray ray = new Ray(new Point(2,-2,3), new Vector(-2,-2,-2));
 
     /**
-     * {@link primitives.Ray#findClosestPoint(List<Point>)}
+     * Test method for {@link primitives.Ray#findClosestPoint(List)}
      */
-    // ============ Equivalence Partitions Tests ==============
-    //TC01: Closest point is in the middle of the list
     @Test
-    void findClosestPoint() {
-        var points = List.of(
-                new Point(0,0,1),
-                new Point(0,-1,0),
-                new Point(1,-2,3),
-                new Point(1,1,1),
-                new Point(1,2,3));
+    void testFindClosestPoint() {
 
-        assertEquals(points.get(points.size() / 2), ray.findClosestPoint(points));
+        Ray ray = new Ray(new Point(0,0,1),new Vector(1, 5, 10));
+
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: the point is in the middle of the list
+        List<Point> list = new LinkedList<>();
+        list.add(new Point(5,25,51));
+        list.add(new Point(3,15,31));
+        list.add(new Point(1,5,11));
+        list.add(new Point(2,10,21));
+
+        assertEquals(list.get(2),ray.findClosestPoint(list), "FindClosestPoint() when point in the middle don't work");
+
+        // =============== Boundary Values Tests ==================
+        // TC02: the list is empty
+        list.clear();
+        assertNull(ray.findClosestPoint(list),"FindClosestPoint() when list equals null don't work");
+
+        // TC03: the point is in the start of the list
+        list = new LinkedList<>();
+        list.add(new Point(1,5,11));
+        list.add(new Point(5,25,51));
+        list.add(new Point(3,15,31));
+        list.add(new Point(2,10,21));
+
+        assertEquals(list.get(0),ray.findClosestPoint(list),"FindClosestPoint() when point in the start don't work");
+
+        // TC04: the point is in the end of the list
+        list = new LinkedList<>();
+        list.add(new Point(5,25,51));
+        list.add(new Point(3,15,31));
+        list.add(new Point(2,10,21));
+        list.add(new Point(1,5,11));
+
+        assertEquals(list.get(3),ray.findClosestPoint(list),"FindClosestPoint() when point in the end don't work");
     }
 
-    // =============== Boundary Values Tests ==================
-    //TC02: gets empty list
-    @Test
-    void findClosestPoint2() {
-        List<Point> points = null;
-        assertNull(ray.findClosestPoint(points));
-    }
-
-    //TC03: Closest point is in the beginning of the list
-    @Test
-    void findClosestPoint3() {
-        var points = List.of(
-                new Point(1,-2,3),
-                new Point(0,-1,0),
-                new Point(1,-2,3),
-                new Point(1,1,1),
-                new Point(1,2,3));
-        assertEquals(points.get(0), ray.findClosestPoint(points));
-    }
-
-    //TC04: Closest point is in the end of the list
-    @Test
-    void findClosestPoint4() {
-        var points = List.of(
-                new Point(1,-2,3),
-                new Point(0,-1,0),
-                new Point(1,-2,3),
-                new Point(1,1,1),
-                new Point(1,-2,3));
-        assertEquals(points.get(points.size()-1), ray.findClosestPoint(points));
-    }
 }
