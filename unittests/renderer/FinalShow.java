1fe9adf8edddb41a125cf685f0399de143e2c3ad
mordechyCohen <non>
2022-06-06 19:54:57 +0300

    mini project 2 - adaptiveSSAA and Multithreading includ JavaDoc - and test for Final Show - all tests run OK - grade 100% !!! ) - 102 including bonuses

--- unittests/renderer/FinalShow.java
+++ unittests/renderer/FinalShow.java
@@ -1,67 +1,64 @@
 package renderer;
 
-import elements.*;
 import geometries.Plane;
 import geometries.Sphere;
 import geometries.Triangle;
+import lighting.AmbientLight;
+import lighting.PointLight;
+import lighting.SpotLight;
 import org.junit.jupiter.api.Test;
-import primitives.Color;
-import primitives.Material;
-import primitives.Point3D;
-import primitives.Vector;
+import primitives.*;
 import scene.Scene;
 
 
 public class FinalShow {
     private Scene scene = new Scene("Test scene");
-    private Camera camera = new Camera(new Point3D(100, 100, 1500), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-            .setViewPlaneSize(200, 200).setDistance(1000);
+    private Camera camera = new Camera(new Point(100, 100, 1500), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+            .setViewPlaneSize(200, 200).setViewPlaneDistance((1000));
 
     @Test
     public void finalShow() {
-        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.15));
+        scene.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), new Double3(0.15)));
 
-        scene.geometries.add( //
-                new Plane(new Point3D(-150, -150, -200),
-                        new Point3D(150, -150, -200),
-                        new Point3D(75, 75, -200))
+        scene.geometries.add(
+                new Plane(new Point(-150, -150, -200),
+                        new Point(150, -150, -200),
+                        new Point(75, 75, -200))
                         .setMaterial(new Material().setKs(0.8).setShininess(60)),
-                new Plane(new Point3D(0, 300, 10),
-                        new Point3D(0, 300, -200),
-                        new Point3D(0, 0, -200))
+                new Plane(new Point(0, 300, 10),
+                        new Point(0, 300, -200),
+                        new Point(0, 0, -200))
                         .setMaterial(new Material().setKs(0.8).setShininess(60)),
-                new Plane(new Point3D(0, 0, 10),
-                        new Point3D(300, 0, -200),
-                        new Point3D(0, 0, -200))
+                new Plane(new Point(0, 0, 10),
+                        new Point(300, 0, -200),
+                        new Point(0, 0, -200))
                         .setMaterial(new Material().setKs(0.8).setShininess(60)),
-                new Plane(new Point3D(300, 0, 20),
-                        new Point3D(200, 0, -200),
-                        new Point3D(200, 300, -200))
+                new Plane(new Point(300, 0, 20),
+                        new Point(200, 0, -200),
+                        new Point(200, 300, -200))
                         .setMaterial(new Material().setKs(0.8).setShininess(60)),
 
 
-                 new Sphere(30, new Point3D(100, 100, -150)) //
+                 new Sphere( new Point(100, 100, -150),30) //
                 .setEmission(new Color(java.awt.Color.BLUE)) //
                 .setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(30)) //
         );
 
         scene.lights.add( //
-                new PointLight(new Color(700, 400, 400), new Point3D(40, 40, 115)) //
+                new PointLight(new Color(700, 400, 400), new Point(40, 40, 115)) //
                         .setKl(4E-4).setKq(2E-5));
         scene.lights.add( //
-                new SpotLight(new Color(700, 400, 400), new Point3D(5, 5, -195),new Vector(1,1,1)) //
+                new SpotLight(new Color(700, 400, 400), new Point(5, 5, -195),new Vector(1,1,1)) //
                         .setKl(4E-4).setKq(2E-5));
         scene.lights.add( //
-                new SpotLight(new Color(700, 400, 400), new Point3D(195, 5, -195),new Vector(-1,1,1)) //
+                new SpotLight(new Color(700, 400, 400), new Point(195, 5, -195),new Vector(-1,1,1)) //
                         .setKl(4E-4).setKq(2E-5));
 
-        Render render = new Render() //
-                .setImageWriter(new ImageWriter("aaaFinal", 600, 600)) //
-                .setCamera(camera) //
-                .setRayTracer(new BasicRayTracer(scene))
+                ImageWriter imageWriter= new ImageWriter("Final", 600, 600); //
+                camera.setImageWriter(imageWriter)
+                .setRayTracer(new RayTracerBasic(scene)).setAdaptiveSSAA(10)//.setMultithreading(3)
                 .setMultithreading(3).setDebugPrint()
-                ;
-        render.renderImage();
-        render.writeToImage();
+                .renderImage()
+                .writeToImage();
     }
 }
